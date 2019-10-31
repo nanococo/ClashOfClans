bff54a09a33d97706316c07f993ecf7accd4fb78
JorgeGuty <49891177+JorgeGuty@users.noreply.github.com>
2019-10-31 14:06:11 -0600

    va de nuevo

--- core/src/com/mygdx/clashofclans/Tokens/Warriors/Characters/Bichillos.java
+++ core/src/com/mygdx/clashofclans/Tokens/Warriors/Characters/Bichillos.java
@@ -3,7 +3,13 @@ package com.mygdx.clashofclans.Tokens.Warriors.Characters;
 import com.mygdx.clashofclans.Tokens.Warriors.Aerial;
 
 public class Bichillos extends Aerial {
+
+    private static String PATH_TO_IDLE_ANIMATION = "Hector/Hector (Idle).gif";
+    private static String PATH_TO_WALKING_ANIMATION = "Hector/Hector (Walking).gif";
+    private static String PATH_TO_ATTACK_ANIMATION = "Hector/Hector (Attack).gif";
+    private static String PATH_TO_HURT_ANIMATION = "Hector/Hector (Hurt).gif";
+
     public Bichillos(int pInitialX, int pInitialY) {
-        super(pInitialX, pInitialY);
+        super(pInitialX, pInitialY, new String[]{PATH_TO_IDLE_ANIMATION, PATH_TO_WALKING_ANIMATION, PATH_TO_ATTACK_ANIMATION, PATH_TO_HURT_ANIMATION});
     }
 }
