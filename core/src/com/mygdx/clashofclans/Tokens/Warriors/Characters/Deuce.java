5c9132dd13c5ca84e3a21cfb4c10fd42b0a39ce2
JorgeGuty <49891177+JorgeGuty@users.noreply.github.com>
2019-10-31 14:10:34 -0600

    Cada quin con su respectiva animacion

--- core/src/com/mygdx/clashofclans/Tokens/Warriors/Characters/Deuce.java
+++ core/src/com/mygdx/clashofclans/Tokens/Warriors/Characters/Deuce.java
@@ -4,10 +4,10 @@ import com.mygdx.clashofclans.Tokens.Warriors.Contact;
 
 public class Deuce extends Contact {
 
-    private static String PATH_TO_IDLE_ANIMATION = "Hector/Hector (Idle).gif";
-    private static String PATH_TO_WALKING_ANIMATION = "Hector/Hector (Walking).gif";
-    private static String PATH_TO_ATTACK_ANIMATION = "Hector/Hector (Attack).gif";
-    private static String PATH_TO_HURT_ANIMATION = "Hector/Hector (Hurt).gif";
+    private static String PATH_TO_IDLE_ANIMATION = "Deuce/Deuce (Idle).gif";
+    private static String PATH_TO_WALKING_ANIMATION = "Deuce/Deuce (Walking).gif";
+    private static String PATH_TO_ATTACK_ANIMATION = "Deuce/Deuce (Attack).gif";
+    private static String PATH_TO_HURT_ANIMATION = "Deuce/Deuce (Hurt).gif";
 
     public Deuce(int pInitialX, int pInitialY) {
         super(pInitialX, pInitialY, new String[]{PATH_TO_IDLE_ANIMATION, PATH_TO_WALKING_ANIMATION, PATH_TO_ATTACK_ANIMATION, PATH_TO_HURT_ANIMATION});
--- core/src/com/mygdx/clashofclans/Tokens/Warriors/Characters/Yolanda.java
+++ core/src/com/mygdx/clashofclans/Tokens/Warriors/Characters/Yolanda.java
@@ -4,10 +4,10 @@ import com.mygdx.clashofclans.Tokens.Warriors.MidRange;
 
 public class Yolanda extends MidRange {
 
-    private static String PATH_TO_IDLE_ANIMATION = "Hector/Hector (Idle).gif";
-    private static String PATH_TO_WALKING_ANIMATION = "Hector/Hector (Walking).gif";
-    private static String PATH_TO_ATTACK_ANIMATION = "Hector/Hector (Attack).gif";
-    private static String PATH_TO_HURT_ANIMATION = "Hector/Hector (Hurt).gif";
+    private static String PATH_TO_IDLE_ANIMATION = "Yolanda/Yolanda (Idle).gif";
+    private static String PATH_TO_WALKING_ANIMATION = "Yolanda/Yolanda (Walking).gif";
+    private static String PATH_TO_ATTACK_ANIMATION = "Yolanda/Yolanda (Attack).gif";
+    private static String PATH_TO_HURT_ANIMATION = "Yolanda/Yolanda (Hurt).gif";
 
     public Yolanda(int pInitialX, int pInitialY) {
         super(pInitialX, pInitialY, new String[]{PATH_TO_IDLE_ANIMATION, PATH_TO_WALKING_ANIMATION, PATH_TO_ATTACK_ANIMATION, PATH_TO_HURT_ANIMATION});
